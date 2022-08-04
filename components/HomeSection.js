9b117947d6e573ed81d33c17d771093cc43e33f1
Kamal <kamalammouri1994@gmail.com>
2022-08-04 16:21:37 +0100

    ...

--- components/HomeSection.js
+++ components/HomeSection.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { homeSectionItems } from "../data/homeSectionItems";
 
 const HomeSection = () => {
-    let pcImage = "";
-    let mobileImage = "";
+    const [pcImage, setPcImage] = useState(homeSectionItems[0].image);
+    const [mobileImage, setMobileImage] = useState(homeSectionItems[0].subimage);
+
+
+    const selectedItem = (item) => {
+        setPcImage(item.image);
+        setMobileImage(item.subimage);
+    };
     return (
         <>
         <section className="primary-bg py-5">
@@ -10,10 +17,10 @@ const HomeSection = () => {
                 <div className="row justify-content-center align-items-center">
                     <div className="col-2">
                         <ul className="list-unstyled">
-                            {homeSectionItems.map((item, index) => {
+                            {homeSectionItems.map((item,index) => {
                                 return (
                                     <>
-                                    <li className="list-item p-1 my-3" key={index}>
+                                    <li className="list-item p-1 my-3" key={index} onClick={()=>{selectedItem(item)}}>
                                         <span className="text-muted">{item.label}</span>
                                     </li>
                                     </>
@@ -26,11 +33,11 @@ const HomeSection = () => {
                         <div className="row position_relative">
                             <div className="col-12">
                                 <img className="img-fluid desktop-mask" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e004f6310f703dde447b_Desktop-p-1600.png" alt=""/>
-                                <img className="img-fluid desktop-content" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/62988cb4cb60e75de1f44e0d_Desktop%20-%20Wiki-p-1080.jpeg" alt=""/>
+                                <img className="img-fluid desktop-content" src={pcImage} alt=""/>
                             </div>
                             <div className="col-3 phone-container">
                                 <img className="img-fluid phone-mask" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e0537ef59a9e13f893c0_Mobile-p-500.png" alt=""/>
-                                <img className="img-fluid phone-content" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6299ce13229401d05c2bfc7f_Mobile%20-%20Wiki.jpg" alt=""/>
+                                <img className="img-fluid phone-content" src={mobileImage} alt=""/>
                             </div>
                         </div>
                     </div>
