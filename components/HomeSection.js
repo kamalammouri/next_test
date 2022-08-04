625af6f20edd4071b8af7fc416feacacf9c4222c
Kamal <kamalammouri1994@gmail.com>
2022-08-04 15:52:59 +0100

    img fix

--- components/HomeSection.js
+++ components/HomeSection.js
@@ -1,6 +1,8 @@
 import { homeSectionItems } from "../data/homeSectionItems";
 
 const HomeSection = () => {
+    let pcImage = "";
+    let mobileImage = "";
     return (
         <>
         <section className="primary-bg py-5">
@@ -23,10 +25,12 @@ const HomeSection = () => {
                     <div className="col-10">
                         <div className="row position_relative">
                             <div className="col-12">
-                                <img className="img-fluid" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e004f6310f703dde447b_Desktop-p-1600.png" alt=""/>
+                                <img className="img-fluid desktop-mask-image" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e004f6310f703dde447b_Desktop-p-1600.png" alt=""/>
+                                <img className="img-fluid desktop-screenshot" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/62988cb4cb60e75de1f44e0d_Desktop%20-%20Wiki-p-1080.jpeg" alt=""/>
                             </div>
-                            <div className="col-3 phone-cadr">
-                                <img className="img-fluid" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e0537ef59a9e13f893c0_Mobile-p-500.png" alt=""/>
+                            <div className="col-3 phone-container">
+                                <img className="img-fluid phone-mask" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e0537ef59a9e13f893c0_Mobile-p-500.png" alt=""/>
+                                <img className="img-fluid phone-content" src="https://assets-global.website-files.com/5f84417443f34a30444f52d3/6299ce13229401d05c2bfc7f_Mobile%20-%20Wiki.jpg" alt=""/>
                             </div>
                         </div>
                     </div>
--- data/homeSectionItems.js
+++ data/homeSectionItems.js
@@ -1,8 +1,8 @@
 export const homeSectionItems = [
     {
         label: 'Wiki',
-        image: 'https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e004f6310f703dde447b_Desktop-p-1600.png',
-        subimage: 'https://assets-global.website-files.com/5f84417443f34a30444f52d3/6290e0537ef59a9e13f893c0_Mobile-p-500.png',
+        image: 'https://assets-global.website-files.com/5f84417443f34a30444f52d3/62988cb4cb60e75de1f44e0d_Desktop%20-%20Wiki-p-1080.jpeg',
+        subimage: 'https://assets-global.website-files.com/5f84417443f34a30444f52d3/6299ce13229401d05c2bfc7f_Mobile%20-%20Wiki.jpg',
     },
     {
         label: 'Projects',
